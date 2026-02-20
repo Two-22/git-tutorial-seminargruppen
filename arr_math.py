3a16871184ae03e475b5694c4d4211dc2a613e24
blackblack-berry <helene_emilia.hausmann@mailbox.tu-dresden.de>
2026-02-20 11:46:58 +0100

    reintroduced sum, sum and mult now in different functions

--- arr_math.py
+++ arr_math.py
@@ -1,4 +1,10 @@
 def sum_array(arr):
+    total = 0
+    for num in arr:
+        total += num
+    return total
+
+def mult_array(arr):
     total = 1
     for num in arr:
         total *= num
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import random
-from arr_math import sum_array
+from arr_math import sum_array, mult_array
 #from arr_sort import bubble_sort
 
 random_array = [random.randint(0, 10) for _ in range(5)]
 
-total = sum_array(random_array)
+total_sum = sum_array(random_array)
+total_mult = mult_array(random_array)
 #arr_sort = bubble_sort(random_array)
 
 print(f"Generated Array:\n {random_array}\n")
-print(f"Produkt: {total}\n")
+print(f"Sum: {total_sum}\n")
+print(f"Mult: {total_mult}\n")
 #print(f"Sorted:\n {arr_sort}\n")
